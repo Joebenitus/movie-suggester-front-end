eaea54ad03f3706a461f48a665ecb318043a4d57
Joseph Karnafel <josephkarnafel@gmail.com>
2020-12-15 11:21:08 -0800

    convert makeApiCall method to action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const requestMovies = () => ({
   type: c.REQUEST_MOVIES
 });
 
-export const getAllMoviesSuccess = (movies) => ({
+export const getAllMoviesSSuccess = (movies) => ({
   type: c.GET_ALL_MOVIES_SUCCESS,
   movies
 });
@@ -13,3 +13,18 @@ export const getAllMoviesFailure = (error) => ({
   type: c.GET_ALL_MOVIES_FAILURE,
   error
 });
+
+export const makeApiCall = () => {
+  return dispatch => {
+    dispatch(requestMovies);
+    return fetch(`http://localhost:3000/movies`)
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+        dispatch(getAllMoviesSuccess(jsonifiedResponse))
+      })
+      .catch((error) => {
+        dispatch(getAllMoviesFailure(error));
+      });
+  }
+  
+}
